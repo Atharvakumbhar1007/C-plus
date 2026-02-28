030c7702b8aa3607807a7fbafa6a6107576659b6
Atharva <atharvakumbhar631@gmail.com>
2026-02-28 16:57:00 +0530

    Create arithematic.cpp

---
+++ CLASS/arithematic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 5;
+
+    cout << x + y << "\n";
+    cout << (x - y) << "\n";
+    cout << (x * y) << "\n";
+    cout << (x / y) << "\n";
+    cout << (x % y) << "\n";
+
+    int z = 5;
+    ++z;
+    cout << z << "\n";
+    --z;
+    cout << z << "\n";
+
+    return 0;
+}

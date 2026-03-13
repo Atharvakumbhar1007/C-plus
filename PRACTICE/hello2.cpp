af7f8022127265100a28b91fad766c695abc60a3
Atharva <atharvakumbhar631@gmail.com>
2026-03-13 11:51:27 +0530

    getline

---
+++ PRACTICE/hello2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    long b = 159;
+    long long c = 1500000000000000;
+    float d = 5.98875;
+    double e = 45;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+
+    return 0;
+}

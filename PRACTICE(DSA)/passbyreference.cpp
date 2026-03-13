3c8cc37b8d0dc9cb4b543f17a852f8d726d0d5da
Atharva <atharvakumbhar631@gmail.com>
2026-03-13 16:36:22 +0530

    Pass by value and reference

---
+++ PRACTICE(DSA)/passbyreference.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+// pass by value
+void doSomething(string &s)
+{
+    s[0] = 't';
+    cout << s << endl;
+}
+int main()
+{
+    string s = "raj";
+    doSomething(s);
+    cout << s << endl;
+    return 0;
+}

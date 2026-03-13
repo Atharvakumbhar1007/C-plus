83a5ee5b187e735fe7c94ecbeb8edb3fecceaf87
Atharva <atharvakumbhar631@gmail.com>
2026-03-13 16:02:43 +0530

    for loop

---
+++ PRACTICE(DSA)/DAY1/for.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;
+
+    cout << "Striver" << endl;
+
+    for (int i = 1; i <= 10; i + 1)
+    {
+        cout << "Striver" << endl;
+    }
+    cout << i << endl;
+    return 0;
+}

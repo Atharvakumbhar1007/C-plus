280aed456bea01b0e1d72f560e62dfeddfa779d8
Atharva <atharvakumbhar631@gmail.com>
2026-03-13 16:09:22 +0530

    do while loop and while loop

---
+++ PRACTICE(DSA)/DAY1/dowhile.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i = 2;
+    while (i <= 5)
+    {
+        cout << "Stiver" << i << endl;
+        i = i + 1;
+    }
+    while (i <= 1)
+        ;
+    {
+        cout << 1 << endl;
+    }
+
+    return 0;
+}

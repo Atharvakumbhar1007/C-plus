841fd442ba44e2feb5b6c10755268401378ff0ec
Atharva <atharvakumbhar631@gmail.com>
2026-03-19 17:03:36 +0530

    vectors

--- PRACTICE(DSA)/DAY2/vector.cpp
+++ PRACTICE(DSA)/DAY2/vector.cpp
@@ -1,12 +1,27 @@
-vector<int>::iterator it = v.begin();
-it++;
-cout << *(it) << "";
+#include <iostream>
+#include <vector>
+using namespace std;
 
-it = it + 2;
-cout << *(it) << " ";
-vector<int>::iterator it = v.end();
-vector<int>::iterator it = v.rend();
-vector<int>::iterator it = v.rbegin();
+int main()
+{
+    vector<int> v = {10, 20, 30, 40, 50};
 
-cout << [0] << " " << v.at(0);
-cout << v.back() << " ";
+    vector<int>::iterator it = v.begin();
+
+    it++;
+    cout << *(it) << " "; // 20
+
+    it = it + 2;
+    cout << *(it) << " "; // 40
+
+    it = v.end(); // points after last (don't dereference)
+
+    vector<int>::reverse_iterator rit = v.rbegin();
+    cout << *rit << " "; // 50
+
+    cout << v[0] << " " << v.at(0) << " "; // 10 10
+
+    cout << v.back() << " "; // 50
+
+    return 0;
+}

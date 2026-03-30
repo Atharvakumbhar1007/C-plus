7f8976dfad2cd21cd83d18a2530a0aaf534e3e33
Atharva <atharvakumbhar631@gmail.com>
2026-03-30 14:49:13 +0530

    Pattrens

---
+++ PRACTICE(DSA)/DAY3/pat1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    printPattern();
+    return 0;
+}
